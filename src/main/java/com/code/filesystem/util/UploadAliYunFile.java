b3a5e2bfff4eace2508d9cfb18c1937907b74f8a
MaJian <1039510196@qq.com>
2018-05-07 08:55:01 +0800

    no message

--- src/main/java/com/code/filesystem/util/UploadAliYunFile.java
+++ src/main/java/com/code/filesystem/util/UploadAliYunFile.java
@@ -31,34 +31,4 @@ public class UploadAliYunFile {
 		ossClient.shutdown();
 		return "https://" +oss.BucketName+"."+ oss.Endpoint+"/"+path;
 	}
-
-
-//	/**
-//	 * 文件上传
-//	 * @param inputStream
-//	 * @param
-//	 * @return
-//	 * @throws FileNotFoundException
-//	 */
-//	public static String UploadAliYunBase64Service(WeChatInfo weChatInfo, InputStream inputStream) throws FileNotFoundException{
-//		String path =  "";
-//		if(weChatInfo!=null) {
-//			String DirName="Pictures";
-//			final UUID uuid = UUID.randomUUID();
-//			final String fileNameUp=uuid.randomUUID().toString()+".png";
-//			path=DirName+"/"+SmBaseGlobal.sdfDate.format(new Date())+"/"+fileNameUp;
-//			// endpoint以杭州为例，其它region请按实际情况填写
-//			String endpoint = "https://"+weChatInfo.getAccessToken();
-//			// accessKey请登录https://ak-console.aliyun.com/#/查看
-//			String accessKeyId = weChatInfo.getAppID();
-//			String accessKeySecret = weChatInfo.getAppsecret();
-//			// 创建OSSClient实例
-//			OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
-//			ossClient.putObject(weChatInfo.getTicket(), path, inputStream);
-//			// 关闭client
-//			ossClient.shutdown();
-//		}
-//		//System.out.println("http://readbank.oss-cn-hangzhou.aliyuncs.com/"+path);
-//		return "https://" +weChatInfo.getTicket()+"."+ weChatInfo.getAccessToken()+"/"+path;
-//	}
 }
